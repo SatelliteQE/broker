50720366d74477ff64cc8c95321ab238dfbc6f6a
Mike Shriver <mshriver@redhat.com>
2020-05-21 09:56:54 -0400

    Update log level setting to allow any level

--- broker/logger.py
+++ broker/logger.py
@@ -6,26 +6,17 @@ import logzero
 
 def setup_logzero(level="info", path="logs/broker.log"):
     log_fmt = "%(color)s[%(levelname)s %(asctime)s]%(end_color)s %(message)s"
-    if level == "debug":
-        level = logging.DEBUG
-        log_fmt = (
-            "%(color)s[%(levelname)1.1s %(asctime)s %(module)s:%(lineno)d]"
-            "%(end_color)s %(message)s"
-        )
-    elif level == "info":
-        level = logging.INFO
-    elif level == "warning":
-        level = logging.WARNING
-    elif level == "error":
-        level = logging.ERROR
-    elif level == "critical":
-        level = logging.CRITICAL
+    debug_fmt = (
+        "%(color)s[%(levelname)1.1s %(asctime)s %(module)s:%(lineno)d]"
+        "%(end_color)s %(message)s"
+    )
+    log_level = getattr(logging, level.upper(), logging.INFO)
 
-    formatter = logzero.LogFormatter(fmt=log_fmt)
+    formatter = logzero.LogFormatter(fmt=debug_fmt if log_level is logging.DEBUG else log_fmt)
     logzero.setup_default_logger(formatter=formatter)
-    logzero.loglevel(level)
+    logzero.loglevel(log_level)
     logzero.logfile(
-        path, loglevel=level, maxBytes=1e9, backupCount=3, formatter=formatter
+        path, loglevel=log_level, maxBytes=1e9, backupCount=3, formatter=formatter
     )
 
 
