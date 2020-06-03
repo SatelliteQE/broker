e4d82df7bc1cc65e0dffe35af6569f3d44108db0
Jacob Callahan <jacob.callahan05@gmail.com>
2020-06-03 12:02:44 -0400

    Add execute subcommand to broker + Added execute functionality to broker + Added more functionality to VMBroker subclass to handle execute + Slightly changed AnsibleTower provider to allow for arbitrary workflow execution

--- broker/commands.py
+++ broker/commands.py
@@ -137,3 +137,46 @@ def duplicate(vm, all_):
                 logger.warning(
                     f"Unable to duplicate {host['hostname']}, no _broker_args found"
                 )
+
+
+@cli.command(
+    context_settings={"allow_extra_args": True, "ignore_unknown_options": True}
+)
+@click.option("--workflow", type=str)
+@click.option("--nick", type=str, help="Use a nickname defined in your settings")
+@click.option("--output-format", "-o", type=click.Choice(["log", "raw", "yaml"]), default="log")
+@click.option("--artifacts", is_flag=True, help="AnsibleTower: return all artifacts associated with the execution.")
+@click.pass_context
+def execute(ctx, workflow, nick, output_format, artifacts):
+    """Execute an arbitrary provider action
+    COMMAND: broker execute --workflow "workflow-name" --workflow-arg1 something
+    or
+    COMMAND: broker execute --nick "nickname"
+
+    :param ctx: clicks context object
+
+    :param workflow: workflow template stored in Ansible Tower, passed in as a string
+
+    :param nick: shortcut for arguments saved in settings.yaml, passed in as a string
+
+    :param output_format: change the format of the output to one of the choice options
+
+    :param artifacts: AnsibleTower provider specific option for choosing what to return
+    """
+    broker_args = {}
+    if nick:
+        broker_args["nick"] = nick
+    if workflow:
+        broker_args["workflow"] = workflow
+    if artifacts:
+        broker_args["artifacts"] = True
+    # if additional arguments were passed, include them in the broker args
+    broker_args.update(dict(zip(ctx.args[::2], ctx.args[1::2])))
+    broker_inst = VMBroker(**broker_args)
+    result = broker_inst.execute()
+    if output_format == "raw":
+        print(result)
+    elif output_format == "log":
+        logger.info(result)
+    elif output_format == "yaml":
+        print(helpers.yaml_format(result))
