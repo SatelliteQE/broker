0874aaff6bef514ddc628a4b7cbaa5ca988acde9
Jacob Callahan <jacob.callahan05@gmail.com>
2020-08-12 13:58:29 -0400

    Adding dynaconf validators to broker settings This change intends to make it more clear to users when their settings need to be changed. Additionally, some sane defaults can be added if missing.

--- broker/settings.py
+++ broker/settings.py
@@ -1,6 +1,9 @@
 import os
+import sys
 from pathlib import Path
-from dynaconf import Dynaconf
+from dynaconf import Dynaconf, Validator
+from dynaconf.validator import ValidationError
+from logzero import logger
 
 settings_file = "broker_settings.yaml"
 BROKER_DIRECTORY = Path()
@@ -10,4 +13,24 @@ if "BROKER_DIRECTORY" in os.environ:
     if envar_location.is_dir():
         BROKER_DIRECTORY = envar_location
 
-settings = Dynaconf(settings_file=str(BROKER_DIRECTORY.joinpath("broker_settings.yaml")))
+settings_path = BROKER_DIRECTORY.joinpath("broker_settings.yaml")
+must_exist = [
+    "ANSIBLETOWER.base_url",
+    "ANSIBLETOWER.username",
+    "ANSIBLETOWER.password",
+    "NICKS",
+    "HOST_PASSWORD",
+]
+validators = [
+    Validator(*must_exist, must_exist=True),
+    Validator("ANSIBLETOWER.release_workflow", default="remove-vm"),
+    Validator("ANSIBLETOWER.extend_workflow", default="extend-vm"),
+    Validator("ANSIBLETOWER.workflow_timeout", is_type_of=int, default=3600),
+    Validator("HOST_USERNAME", default="root"),
+]
+settings = Dynaconf(settings_file=str(settings_path), validators=validators,)
+try:
+    settings.validators.validate()
+except ValidationError as err:
+    logger.error(f"Configuration error in {settings_path.absolute()}: {err}")
+    sys.exit()
