85ada1ddba78bc9e98f75576f8c474ac0e602ef5
Mike Shriver <mshriver@redhat.com>
2021-06-25 09:03:00 -0400

    Release concurrent checkins

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ extras = {
 
 setup(
     name="broker",
-    version="0.1.19",
+    version="0.1.20",
     description="The infrastructure middleman.",
     long_description=readme + "\n\n" + history,
     long_description_content_type="text/markdown",
