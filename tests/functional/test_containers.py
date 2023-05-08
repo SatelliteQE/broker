a2529a8683c696583552761c12c9dca4afb13822
Jacob Callahan <jacob.callahan05@gmail.com>
2023-05-08 17:00:46 -0400

    Add a tail_file context manager to the Host class
    This is a pretty common request and should be useful in a number of
    scenarios. Usage is pretty simple:
    
        with my_host.session.tail_file("/var/log/messages") as res:
            # do something that creates new messages
        print(res.stdout)

--- tests/functional/test_containers.py
+++ tests/functional/test_containers.py
@@ -89,6 +89,13 @@ def test_container_e2e():
                 loc_settings_path.read_bytes() == data
             ), "Local file is different from the received one (return_data=True)"
             assert data == Path(tmp.file.name).read_bytes(), "Received files do not match"
+        # test the tail_file context manager
+        tailed_file = f"{remote_dir}/tail_me.txt"
+        c_host.execute(f"echo 'hello world' > {tailed_file}")
+        with c_host.session.tail_file(tailed_file) as tf:
+            c_host.execute(f"echo 'this is a new line' >> {tailed_file}")
+        assert 'this is a new line' in tf.stdout
+        assert 'hello world' not in tf.stdout
 
 
 def test_container_e2e_mp():
