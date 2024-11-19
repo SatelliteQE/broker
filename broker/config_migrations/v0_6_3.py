5213beb3be23d2845c3f2c55fa8e1bd8a44b88d1
Jacob Callahan <jacob.callahan05@gmail.com>
2024-11-19 16:21:37 -0500

    Add initial attempts to automatically clean up dangling hosts
    Now, when workflows fail, Broker will attempt to find a handgling host
    and check it in if found.

---
+++ broker/config_migrations/v0_6_3.py
@@ -0,0 +1,22 @@
+"""Config migrations for versions older than 0.6.3 to 0.6.3."""
+
+from logzero import logger
+
+TO_VERSION = "0.6.3"
+
+
+def add_dangling_behavior(config_dict):
+    """Add the dangling_behavior config to AnsibleTower."""
+    if "AnsibleTower" in config_dict:
+        if "dangling_behavior" not in config_dict["AnsibleTower"]:
+            logger.debug("Adding dangling_behavior to AnsibleTower.")
+            config_dict["AnsibleTower"]["dangling_behavior"] = "checkin"
+    return config_dict
+
+
+def run_migrations(config_dict):
+    """Run all migrations."""
+    logger.info(f"Running config migrations for {TO_VERSION}.")
+    config_dict = add_dangling_behavior(config_dict)
+    config_dict["_version"] = TO_VERSION
+    return config_dict
